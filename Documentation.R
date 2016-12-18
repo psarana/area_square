da63f4671b8f8c78c5cc203df880e52e578606d4
Parveen Sarana <parveenkaursarana@gmail.com>
2016-12-17 23:48:44 -0800

    add documentation for class

---
+++ Documentation.R
@@ -0,0 +1,8 @@
+#' An S4 class to convert grams to liters
+#'
+#' Convert Grams(g) to Liters(l)
+#'
+#' @param x A number in g
+#' @return The number of \code{x} in Liters
+#' @examples
+#' liters(250)
