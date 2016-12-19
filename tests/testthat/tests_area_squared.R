1a2b834d8d96ddd8e408c1931685428f8bc54fb6
Parveen Sarana <parveenkaursarana@gmail.com>
2016-12-18 22:49:57 -0800

    update for strings

--- tests/testthat/tests_area_squared.R
+++ tests/testthat/tests_area_squared.R
@@ -4,17 +4,16 @@ test_that("area_square calculates the area of a given square correctly"){
   
   expect_identical(area.my_square(my_square(3)), 9)
   expect_identical(area.my_square(my_square(10)), 100)
-  expect_identical(area.my_square(my_square(7), 49))
+  expect_identical(area.my_square(my_square(7)), 49)
                    
   #unusual inputs
   expect_identical(area.my_square(my_square(0), 0))
   
   #expected errors
-  expect_error(standard_deviation(c(), " Error in my_square() : argument "a" is missing, with no default "))
-  expect_error(standard_deviation(c(1,2), "  Error in my_square(1, 2) : unused argument (2)  "))
-  
+  expect_error(area.my_square(my_square(c())), " Error in my_square() : argument "a" is missing, with no default ")
+  expect_error(area.my_square(my_square((c(1,2)), "Error in my_square(1, 2) : unused argument (2)")))
+  expected_error(area.my_square(my_square("hello")), "Error in (obj$side)^2 : non-numeric argument to binary operator")
 
-  
   
 }
 
